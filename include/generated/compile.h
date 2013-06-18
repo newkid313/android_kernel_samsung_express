a41d0b5c14f4f0c70b4dc10d72fb31247f049e38
Czar Dixon <rbheromax@rbheromax-dell.(none)>
2013-06-18 08:56:57 -0400

    upload of stock ICS src

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Jun 18 08:33:34 EDT 2013"
+#define LINUX_COMPILE_BY "rbheromax"
+#define LINUX_COMPILE_HOST "rbheromax-dell"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
